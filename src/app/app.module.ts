e01e7c04625f08c7e2fdfb4adeaa0285f8712194
Андрей Шемяков <ashemyakov@diasoft.ru>
2018-01-25 08:06:51 +0300

    Страница логина. Получение данных с сервера

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import {HttpModule} from '@angular/http';
 import {AuthModule} from './auth/auth.module';
 import {AppRoutingModule} from './app-routing.module';
+import {UsersService} from './shared/services/users.service';
 
 
 @NgModule({
@@ -17,7 +18,7 @@ import {AppRoutingModule} from './app-routing.module';
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
