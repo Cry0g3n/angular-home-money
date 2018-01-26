2bd658db1fef13ff02cd3cdc5d626bc7d4238432
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2018-01-26 22:46:38 +0300

    Создание левого меню

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -7,6 +7,7 @@ import {HistoryPageComponent} from './history-page/history-page.component';
 import {PlanningPageComponent} from './planning-page/planning-page.component';
 import {RecordsPageComponent} from './records-page/records-page.component';
 import {SystemComponent} from './system.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   imports: [
@@ -19,7 +20,8 @@ import {SystemComponent} from './system.component';
     BillPageComponent,
     HistoryPageComponent,
     PlanningPageComponent,
-    RecordsPageComponent
+    RecordsPageComponent,
+    SidebarComponent
   ]
 })
 export class SystemModule {
