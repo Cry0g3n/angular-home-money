42abdeb700ca1839ccae190423491a3dacd9f08d
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2018-01-27 19:35:56 +0300

    Страница счета. Получение данных

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+
+import {BillService} from '../shared/services/bill.service';
+import {Bill} from '../shared/models/bill.model';
 
 @Component({
   selector: 'wfm-bill-page',
   templateUrl: './bill-page.component.html',
   styleUrls: ['./bill-page.component.scss']
 })
-export class BillPageComponent implements OnInit {
+export class BillPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+
+  constructor(private billService: BillService) {
+  }
 
   ngOnInit() {
+    this.subscription = Observable.combineLatest(
+      this.billService.getBill(),
+      this.billService.getCurrency()
+    ).subscribe((data: [Bill, any]) => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
