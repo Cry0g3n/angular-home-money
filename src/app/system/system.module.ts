d2310eb4ed12df53f26a695f8a4bc8afeb7d991e
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2018-01-27 17:49:33 +0300

    Создание хедера

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+
 import {SharedModule} from '../shared/shared.module';
 import {SystemRoutingModule} from './system-routing.module';
 import {BillPageComponent} from './bill-page/bill-page.component';
@@ -7,7 +8,9 @@ import {HistoryPageComponent} from './history-page/history-page.component';
 import {PlanningPageComponent} from './planning-page/planning-page.component';
 import {RecordsPageComponent} from './records-page/records-page.component';
 import {SystemComponent} from './system.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
+import {HeaderComponent} from './shared/components/header/header.component';
+import {DropdownDirective} from './shared/directives/dropdown.directive';
 
 @NgModule({
   imports: [
@@ -21,7 +24,9 @@ import { SidebarComponent } from './shared/components/sidebar/sidebar.component'
     HistoryPageComponent,
     PlanningPageComponent,
     RecordsPageComponent,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    DropdownDirective
   ]
 })
 export class SystemModule {
