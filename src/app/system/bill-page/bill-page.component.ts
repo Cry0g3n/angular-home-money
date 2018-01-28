3248bef673dc153a14396bf9f3077463c27235fb
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2018-01-28 10:24:19 +0300

    Страница счета. Вывод данных

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -12,22 +12,42 @@ import {Bill} from '../shared/models/bill.model';
 })
 export class BillPageComponent implements OnInit, OnDestroy {
 
-  subscription: Subscription;
+  sub1: Subscription;
+  sub2: Subscription;
+
+  currency: any;
+  bill: Bill;
+
+  isLoaded = false;
 
   constructor(private billService: BillService) {
   }
 
   ngOnInit() {
-    this.subscription = Observable.combineLatest(
+    this.sub1 = Observable.combineLatest(
       this.billService.getBill(),
       this.billService.getCurrency()
     ).subscribe((data: [Bill, any]) => {
-      console.log(data);
+      this.bill = data[0];
+      this.currency = data[1];
+
+      this.isLoaded = true;
     });
   }
 
+  onRefresh() {
+    this.isLoaded = false;
+    this.sub2 = this.billService.getCurrency()
+      .delay(1000) // TODO: Temp
+      .subscribe((currency: any) => {
+        this.currency = currency;
+        this.isLoaded = true;
+      });
+  }
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
 }
