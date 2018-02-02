57ebee91ce8eac53ec2f28f5a0bc9cd333563db3
Андрей Шемяков <ashemyakov@diasoft.ru>
2018-02-02 08:10:20 +0300

    Защита роутов

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -7,20 +7,24 @@ import {HistoryPageComponent} from './history-page/history-page.component';
 import {PlanningPageComponent} from './planning-page/planning-page.component';
 import {RecordsPageComponent} from './records-page/records-page.component';
 import {HistoryDetailComponent} from './history-page/history-detail/history-detail.component';
+import {AuthGuard} from '../shared/services/auth.guard';
 
 
 const routes: Routes = [
-  {path: '', component: SystemComponent, children: [
+  {
+    path: '', component: SystemComponent, canActivate: [AuthGuard], children: [
       {path: 'bill', component: BillPageComponent},
       {path: 'history', component: HistoryPageComponent},
       {path: 'planning', component: PlanningPageComponent},
       {path: 'records', component: RecordsPageComponent},
       {path: 'history/:id', component: HistoryDetailComponent},
-    ]}
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class SystemRoutingModule {}
+export class SystemRoutingModule {
+}
